b8feedadd886ef2a5520c4493a9841d6ac5e1607
Ehsan <ehsanexe01@gmail.com>
2024-08-19 15:22:55 +0500

    create 11 files and update 8 files

--- models/product.js
+++ models/product.js
@@ -1,33 +1,40 @@
 const fs = require("fs");
 const path = require("path");
 
-const p = path.join(path.dirname(__dirname), "data", "products.json");
+const p = path.join(
+  path.dirname(process.mainModule.filename),
+  "data",
+  "products.json"
+);
 
-const getProductsFromFile = (callBack) => {
-  fs.readFile(p, (err, data) => {
+const getProductsFromFile = (cb) => {
+  fs.readFile(p, (err, fileContent) => {
     if (err) {
-      callBack([]);
-      return;
+      cb([]);
+    } else {
+      cb(JSON.parse(fileContent));
     }
-    callBack(JSON.parse(data));
   });
 };
 
 module.exports = class Product {
-  constructor(title) {
+  constructor(title, imageUrl, description, price) {
     this.title = title;
+    this.imageUrl = imageUrl;
+    this.description = description;
+    this.price = price;
   }
 
   save() {
     getProductsFromFile((products) => {
       products.push(this);
       fs.writeFile(p, JSON.stringify(products), (err) => {
-        console.log({ err });
+        console.log(err);
       });
     });
   }
 
-  static fetchAllProducts(callBack) {
-    getProductsFromFile(callBack);
+  static fetchAll(cb) {
+    getProductsFromFile(cb);
   }
 };
